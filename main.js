9b91a228697ac8043aba4ae10925b763aea6bf34
AsadbekAzizov2002 <azizovasadbek489@gmail.com>
2023-08-23 15:43:14 +0500

    end

---
+++ main.js
@@ -0,0 +1,152 @@
+let user =[
+    {
+        "postId": 1,
+        "id": 1,
+        "name": "id labore ex et quam laborum",
+        "email": "Eliseo@gardner.biz",
+        "body": "laudantium enim quasi est quidem magnam voluptate ipsam eos\ntempora quo necessitatibus\ndolor quam autem quasi\nreiciendis et nam sapiente accusantium"
+      },
+      {
+        "postId": 1,
+        "id": 2,
+        "name": "quo vero reiciendis velit similique earum",
+        "email": "Jayne_Kuhic@sydney.com",
+        "body": "est natus enim nihil est dolore omnis voluptatem numquam\net omnis occaecati quod ullam at\nvoluptatem error expedita pariatur\nnihil sint nostrum voluptatem reiciendis et"
+      },
+      {
+        "postId": 1,
+        "id": 3,
+        "name": "odio adipisci rerum aut animi",
+        "email": "Nikita@garfield.biz",
+        "body": "quia molestiae reprehenderit quasi aspernatur\naut expedita occaecati aliquam eveniet laudantium\nomnis quibusdam delectus saepe quia accusamus maiores nam est\ncum et ducimus et vero voluptates excepturi deleniti ratione"
+      },
+      {
+        "postId": 1,
+        "id": 4,
+        "name": "alias odio sit",
+        "email": "Lew@alysha.tv",
+        "body": "non et atque\noccaecati deserunt quas accusantium unde odit nobis qui voluptatem\nquia voluptas consequuntur itaque dolor\net qui rerum deleniti ut occaecati"
+      },
+      {
+        "postId": 1,
+        "id": 5,
+        "name": "vero eaque aliquid doloribus et culpa",
+        "email": "Hayden@althea.biz",
+        "body": "harum non quasi et ratione\ntempore iure ex voluptates in ratione\nharum architecto fugit inventore cupiditate\nvoluptates magni quo et"
+      },
+      {
+        "postId": 2,
+        "id": 6,
+        "name": "et fugit eligendi deleniti quidem qui sint nihil autem",
+        "email": "Presley.Mueller@myrl.com",
+        "body": "doloribus at sed quis culpa deserunt consectetur qui praesentium\naccusamus fugiat dicta\nvoluptatem rerum ut voluptate autem\nvoluptatem repellendus aspernatur dolorem in"
+      },
+]
+
+// user.forEach ( (id,name,item,index,arr) =>{
+//     console.log(id,name,item,index,arr)
+// });
+
+// const arry1 = [9,8,7,6,5,4,3,2,1];
+
+// console.log(arry1.sort());
+
+
+// const arry1 = [1,2,3];
+// let idf =  arry1.includes
+
+// console.log(idf);
+
+// let arryy = (1,2,3,4,5,6,7,8,9);
+
+// console.log( arryy.slice(2, 4));
+
+
+// let arr =[1,2,[1,2],[1,2],3,4,5,6,7,8];
+
+// console.log(arr.flat());
+
+
+
+// let str = "salom dunyo";
+
+// console.log(str.split("").reverse.join);
+
+
+
+// let studentsData = [["jack",24],["Sora",23],]
+
+// for (let i of studentsData){
+//     for(let j of i){
+//         console.log(j);
+//     }
+// }
+
+
+// let studentsData = [["jack",24],["Sora",23],];
+
+// studentsData.forEach((item)=>{
+//     item.forEach((data)=>{
+//         console.log(data);
+//     });
+// }); 
+
+
+
+
+// let studentsData = [["jack",24],["Sora",23]]
+
+
+// for (let i = 0 ;i < studentsData ; i++){
+//     let innerArrayLength = studentsData[i].length;
+// for(let j =0; j < innerArrayLength; j++){
+
+//     console.log(studentsData[i][j]);
+// }
+// }
+
+
+
+// let str = "Asadbek";
+
+// console.log(str[3]);
+
+
+
+// let str = "salom bolalar";
+
+// console.log(str.replace("bolalar","dangasalar"));
+// console.log(str);
+
+
+// let obj ={
+// //   name:"Asadbek",
+// //   age:21,
+// //   [valau1]:334,
+// // };
+ 
+// // for (let i in obj){
+// //   console.log(obj[i]);
+// // };
+
+
+
+
+
+
+// let user1 =[
+//   name="Asadbek",
+//   age= 21,
+//   ishi= "hozircha bekorchi",
+//   kunlik_harajat= "urtacha 10000 sum"
+  
+// ]
+
+// const x = (arr1,arr2)=> {
+//   let NewUser = [arr1,arr2];
+
+// }
+
+// x(user1,NewUser)
+
+// console.log(NewUser);git 
